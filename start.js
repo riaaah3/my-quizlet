d10d669aea0325df02177f1ff89f060a4a77b2c6
Zariah <zaripull18@gmail.com>
2024-04-19 16:08:33 -0500

    quiz will move on to the next question

--- start.js
+++ start.js
@@ -1,11 +1,17 @@
 const startButton = document.getElementById("start-btn");
 var quizScreen = document.getElementById("quiz-screen");
-console.log(questions)
+var startScreen = document.getElementById("start-screen");
+var questionElement = document.getElementById("quizStart");
+var choiceElements = document.getElementsByClassName("options");
+console.log(questions, choiceElements)
+var score = 0
 startButton.addEventListener("click", startQuiz);
 var questionIndex = 0
+for (let i = 0; i < choiceElements.length; i++) {
+  choiceElements[i].addEventListener("click",checkAnswer)
+}
 function startQuiz() {
-console.log("Starting the quiz now!")
-var startScreen = document.getElementById("start-screen");
+  console.log("Starting the quiz now!")
   startScreen.classList.add("hide")
   quizScreen.classList.remove("hide");
   showQuestion()
@@ -14,33 +20,40 @@ var startScreen = document.getElementById("start-screen");
 function showQuestion() {
   console.log("Show question is running!")
   var currentQuestion = questions[questionIndex]
-  console.log(currentQuestion)
-  var questionElement = document.createElement("h2")
-  console.log(questionElement )
-  questionElement.textContent =currentQuestion.question;
-  console.log(questionElement)
-  quizScreen.appendChild(questionElement)
+  
+  questionElement.innerText = currentQuestion.question;
+  console.log(currentQuestion,questionElement)
+
 
   var choices = currentQuestion.options
 
   for (let i = 0; i < choices.length; i++) {
-    var choiceElement = document.createElement("button")
-    choiceElement.innerHTML = choices[i]
-    quizScreen.append(choiceElement)
-  }
   
+    choiceElements[i].textContent = choices[i]
+   
+  }
+
 }
 
+
+function checkAnswer(event){
+  event.preventDefault()
+  var correctAnswer = event.target.textContent
+  console.log(correctAnswer)
+  if (correctAnswer == questions[questionIndex].answer){
+    score += 5
+  }else{
+
+  }
+  if(questionIndex < questions.length-1){
+    questionIndex++
+    showQuestion()
+  }
+}
 
 var timerEl = document.getElementById('countdown')
 
 function countdown() {
   var timeLeft = 60;
-  
-}
 
-function logValue(x){
-  console.log(x)
 }
-
-logValue(3)
